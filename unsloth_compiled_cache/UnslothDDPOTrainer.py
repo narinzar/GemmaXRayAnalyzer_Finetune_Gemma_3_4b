bc86692d591e4205abf8fad7c34a2b270cd9dc05
narinzar <nari.nzar@gmail.com>
2025-04-02 20:47:26 +0000

    final

--- unsloth_compiled_cache/UnslothDDPOTrainer.py
+++ unsloth_compiled_cache/UnslothDDPOTrainer.py
@@ -145,7 +145,7 @@ class UnslothDDPOConfig(DDPOConfig):
     )
     def __init__(
         self,
-        exp_name = '06_train_model',
+        exp_name = '07_evaluate_model',
         run_name = '',
         seed = 3407,
         log_with = None,
