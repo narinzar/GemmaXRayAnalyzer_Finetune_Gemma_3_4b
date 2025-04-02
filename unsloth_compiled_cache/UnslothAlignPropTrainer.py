b45d9cfe2d1b797bfdbc5c2e05a4be6b86e85221
narinzar <nari.nzar@gmail.com>
2025-04-02 13:20:47 +0000

    fix eev

--- unsloth_compiled_cache/UnslothAlignPropTrainer.py
+++ unsloth_compiled_cache/UnslothAlignPropTrainer.py
@@ -129,7 +129,7 @@ class UnslothAlignPropConfig(AlignPropConfig):
     )
     def __init__(
         self,
-        exp_name = '07_evaluate_model',
+        exp_name = '08_app',
         run_name = '',
         seed = 3407,
         log_with = None,
